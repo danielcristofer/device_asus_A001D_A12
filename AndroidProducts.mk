4d7704017b3c20c1918ba6278d3e0f3e64bde1fc
Daniel Cristofer <149025121+danielcristofer@users.noreply.github.com>
2026-07-22 15:14:41 -0300

    Update AndroidProducts.mk
    Change from Nusantara to crDroid.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_A001D.mk
+    $(LOCAL_DIR)/lineage_A001D.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_A001D-user \
-    nad_A001D-userdebug \
-    nad_A001D-eng
+    lineage_A001D-user \
+    lineage_A001D-userdebug \
+    lineage_A001D-eng
