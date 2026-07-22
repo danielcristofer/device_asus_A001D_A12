762f47e06e2fa7dbaa4d399c5460b18cc58d25c1
Daniel Cristofer <149025121+danielcristofer@users.noreply.github.com>
2026-07-22 16:25:21 -0300

    Update and rename nad_A001D.mk to lineage_A001D.mk
    Change from Nusantara to crDroid

--- lineage_A001D.mk
+++ lineage_A001D.mk
@@ -22,16 +22,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/asus/A001D/device.mk)
 
 # Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common.mk)
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A001D
-PRODUCT_NAME := nad_A001D
+PRODUCT_NAME := lineage_A001D
 PRODUCT_BRAND := ASUS
 PRODUCT_MODEL := ZenFone
 PRODUCT_MANUFACTURER := ASUS
-maintainer_name:="danielcristofer"
 
 # Target
 TARGET_BOOT_ANIMATION_RES := 1080
